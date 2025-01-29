d129e5f2f9f5ffe76fc2c56234f5b5c307924b0b
Ashish Ranjan <ashish@tug.ro>
2025-01-29 12:28:10 +0530

    WIP: Fix config.py - 2

--- suresdk/config.py
+++ suresdk/config.py
@@ -7,7 +7,10 @@ OTEL_RESOURCE = Resource.create()
 class SureSDKConfig:
     DEBUG = False
 
-    ENV = OTEL_RESOURCE.attributes.get(ResourceAttributes.DEPLOYMENT_ENVIRONMENT) or ""
+    ENV = (
+        OTEL_RESOURCE.attributes.get(ResourceAttributes.DEPLOYMENT_ENVIRONMENT)
+        or "prod"
+    )
 
     REPO = OTEL_RESOURCE.attributes.get(ResourceAttributes.SERVICE_NAMESPACE) or ""
     SERVICE = OTEL_RESOURCE.attributes.get(ResourceAttributes.SERVICE_NAME) or ""
@@ -18,7 +21,7 @@ class SureSDKConfig:
         OTEL_RESOURCE.attributes.get(ResourceAttributes.SERVICE_INSTANCE_ID) or ""
     )
 
-    LOG_LEVEL = config_store.get("LOG_LEVEL", "INFO").upper()
+    SERVICE_LOG_LEVEL = config_store.get("SERVICE_LOG_LEVEL", "INFO").upper()
 
     # SSL_CERT_FILE is used to provide path to a custom CA certificate to handle SSL in
     # some libraries, e.g. confluent_kafka, redis
